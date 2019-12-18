35ec25e3bca105ee7605d780292f7ecbeee4e074
m-morishita523 <50609205+m-morishita523@users.noreply.github.com>
2019-12-18 09:21:19 +0900

    Update EkiAPIJsonGet.py

--- EkiAPIJsonGet.py
+++ EkiAPIJsonGet.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "https://api.ekispert.jp/v1/json/search/course/plain?key=eBBWPyXMYduCN759&from=35.70606813177083,139.651624325722,wgs84,2000&to=35.68042009453629,139.7690262983716,wgs84,2000"
+url = "https://api.ekispert.jp/v1/json/search/course/plain?key=**********&from=35.70606813177083,139.651624325722,wgs84,2000&to=35.68042009453629,139.7690262983716,wgs84,2000"
 response = requests.get(url)
 jsonData = response.json()
 print(type(jsonData))
